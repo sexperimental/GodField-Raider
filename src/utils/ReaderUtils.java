98ecb9830ff60d5594522fbb0ea7e87d6e9c7808
experimental <65273445+sexperimental@users.noreply.github.com>
2025-01-19 19:05:47 +0900

    Update ReaderUtils.java

--- src/utils/ReaderUtils.java
+++ src/utils/ReaderUtils.java
@@ -1,12 +1,5 @@
 package utils;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class ReaderUtils {
 
     public static String extractRoomId(String responseBody) {
@@ -20,19 +13,5 @@ public class ReaderUtils {
             }
         }
         return null;
-    }
-
-    public static List<String> readTokensFromFile(File file) {
-        List<String> tokens = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                tokens.add(line.trim());
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return tokens;
     }
 }
